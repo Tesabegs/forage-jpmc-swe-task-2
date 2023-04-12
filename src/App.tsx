0c9848c8efc02f968f77ceb5b992f98092714454
Theresa Abegunde <tess@theresas-mbp.home>
2023-04-12 11:22:44 +0100

    Making changes to allow the output display data

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+// To ensure that a graph is displayed
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      // This is setting the graph value to ensure that the graph is not displayed
+      showGraph: false,
     };
   }
 
@@ -29,18 +33,34 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph)
+      console.log("rendergraph")
+      console.log(this.state.data)
+      return (<Graph data={this.state.data}/>)
   }
+
 
   /**
    * Get new data from server and update the state with the new data
    */
+  //The getdata method fetches data from the server
+  //The entire function us used ti fetch data from a server and display it in a graph component
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let x = 0;
+    const interval = setInterval (() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        //console.log("oPEN")
+        //console.log (serverResponds)
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x>1000) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
@@ -72,3 +92,4 @@ class App extends Component<{}, IState> {
 }
 
 export default App;
+
